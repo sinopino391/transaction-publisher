ef5dac97a5ee15912b242204aec3a84d4641dffb
sinopino391 <120124226+sinopino391@users.noreply.github.com>
2022-12-24 16:35:53 +0000

    Co-authored-by: Enes Poyraz <epoyraz@users.noreply.github.com>

---
+++ transaction-publisher/Program.cs
@@ -0,0 +1,60 @@
+﻿
+using Google.Cloud.PubSub.V1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+public class Program
+{
+    static async Task Main(string[] args)
+    {
+        Environment.SetEnvironmentVariable("GOOGLE_APPLICATION_CREDENTIALS", "/workspaces/transaction-publisher/application_default_credentials.json", EnvironmentVariableTarget.Process);
+        string projectId = "tidy-arena-247812";
+        string topicId = "transactions-websockets";
+        List<string> names = new List<string>();
+
+        // Add some items to the list
+        names.Add("Alice");
+        names.Add("Bob");
+        names.Add("Charlie");
+        Program example = new Program();
+
+        while (true)
+        {
+            // Read a key from the console
+            ConsoleKeyInfo keyInfo = Console.ReadKey();
+
+            // If the key is the spacebar, print "Hello, World!"
+            if (keyInfo.Key == ConsoleKey.Spacebar)
+            {
+                await example.PublishMessagesAsync(projectId, topicId, names);
+            }
+        }
+    }
+    public async Task<int> PublishMessagesAsync(string projectId, string topicId, IEnumerable<string> messageTexts)
+    {
+        TopicName topicName = TopicName.FromProjectTopic(projectId, topicId);
+        PublisherClient publisher = await PublisherClient.CreateAsync(topicName);
+
+        int publishedMessageCount = 0;
+        var publishTasks = messageTexts.Select(async text =>
+        {
+            try
+            {
+                string message = await publisher.PublishAsync(text);
+                Console.WriteLine($"Published message {message}");
+                Interlocked.Increment(ref publishedMessageCount);
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine($"An error ocurred when publishing message {text}: {exception.Message}");
+            }
+        });
+        await Task.WhenAll(publishTasks);
+        return publishedMessageCount;
+    }
+}
+
+
---
+++ transaction-subscriber/Program.cs
@@ -0,0 +1,40 @@
+﻿
+using Google.Cloud.PubSub.V1;
+using System;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+public class Program
+{
+    static async Task Main(string[] args)
+    {
+        Environment.SetEnvironmentVariable("GOOGLE_APPLICATION_CREDENTIALS", "/workspaces/transaction-publisher/application_default_credentials.json", EnvironmentVariableTarget.Process);
+        string projectId = "tidy-arena-247812";
+        string subscriptionId = "transactions-websockets-sub";
+        Program example = new Program();
+
+        await example.PullMessagesAsync(projectId, subscriptionId, true);
+    }
+    public async Task<int> PullMessagesAsync(string projectId, string subscriptionId, bool acknowledge)
+    {
+        SubscriptionName subscriptionName = SubscriptionName.FromProjectSubscription(projectId, subscriptionId);
+        SubscriberClient subscriber = await SubscriberClient.CreateAsync(subscriptionName);
+        // SubscriberClient runs your message handle function on multiple
+        // threads to maximize throughput.
+        int messageCount = 0;
+        Task startTask = subscriber.StartAsync((PubsubMessage message, CancellationToken cancel) =>
+        {
+            string text = System.Text.Encoding.UTF8.GetString(message.Data.ToArray());
+            Console.WriteLine($"Message {message.MessageId}: {text}");
+            Interlocked.Increment(ref messageCount);
+            return Task.FromResult(acknowledge ? SubscriberClient.Reply.Ack : SubscriberClient.Reply.Nack);
+        });
+        // Run for 5 seconds.
+        await Task.Delay(5000);
+        await subscriber.StopAsync(CancellationToken.None);
+        // Lets make sure that the start task finished successfully after the call to stop.
+        await startTask;
+        return messageCount;
+    }
+}
